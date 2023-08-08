4ff4abb07a36d381ca141e53abc66ff6dc76184b
s0ham075 <120611768+s0ham075@users.noreply.github.com>
2023-08-08 16:44:00 +0530

    REACT style data rendering in PURE JS !!!
    RESPECT TO ALL THE GREAT NINJAS WHO FOUGHT IN THE 4th Great Ninja War

---
+++ menu project/menu.js
@@ -0,0 +1,314 @@
+const akamaru={
+    img:"akamaru.jpg",
+    title:"AKAMARU",
+    rank:"chunin",
+    des:" A loyal and intelligent ninja dog who fights alongside his partner Kiba and will do anything to protect his friends."
+};
+const choji={
+    img:"choji.jpg",
+    title:"CHOJJI",
+    rank:"chunin",
+    des:"A kind-hearted and food-loving ninja who has a tough time keeping up with his more skilled teammates, but proves to be a valuable asset in battles with his incredible strength."
+};
+const darui={
+    img:"darui.jpg",
+    title:"DARUI",
+    rank:"jonin",
+    des:"A skilled ninja from the Hidden Cloud Village who wields powerful lightning-style jutsu and is known for his calm and collected demeanor."
+};
+const deidara={
+    img:"deidara.jpg",
+    title:"DIEDARA",
+    rank:"ROGUE",
+    des:" A member of the notorious Akatsuki organization who possesses the ability to create explosive clay sculptures and has a love for art."
+};
+const gaara={
+    img:"gaara.jpg",
+    title:"GAARA",
+    rank:"KAZEKAGE",
+    des:"The former jinchuriki of the One-Tailed Shukaku, and the current Kazekage of the Hidden Sand Village, who starts off as a cold and merciless fighter, but eventually learns to open up to others and become a protector of his village."
+};
+const hinata={
+    img:"hinata.jpg",
+    title:"HINATA",
+    rank:"chunin",
+    des:" A shy and gentle ninja from the Hidden Leaf Village who possesses the powerful Byakugan ability and develops strong feelings for Naruto, inspiring her to become a stronger and more confident fighter."
+};
+const ino={
+    img:"ino.jpg",
+    title:"INO",
+    rank:"chunin",
+    des:"A skilled kunoichi from the Hidden Leaf Village who specializes in mind-based jutsu and uses her abilities to support her teammates in battles, while also running her family's flower shop."
+};
+const itachi={
+    img:"itachi.jpg",
+    title:"ITACHI SAMA",
+    rank:"GOD",
+    des:" A powerful and mysterious ninja from the Hidden Leaf Village, who was initially believed to have betrayed his clan and became a member of the Akatsuki"
+};
+const kankuro={
+    img:"kankuro.jpg",
+    title:"KANKURO",
+    rank:"jonin",
+    des:"A puppet-using ninja from the Hidden Sand Village, and the older brother of Gaara, who initially has a competitive and brash personality, but learns to become more responsible and caring towards his family and comrades."
+};
+const killer_bee ={
+    img:"killer bee sama.jpg",
+    title:"KILLER BEE",
+    rank:"JINCHURIKI",
+    des:"A skilled ninja from the Hidden Cloud Village, and the jinchuriki of the Eight-Tailed Ox, who is known for his impressive rapping skills and fighting style, which involves the use of seven swords and his powerful chakra."
+};
+const kisame ={
+    img:"kisame.jpg",
+    title:"KISAME",
+    rank:"ROGUE",
+    des:"A former member of the Seven Swordsmen of the Mist and a member of the Akatsuki, known for his brutal fighting style and his ability to transform into a shark-human hybrid."
+};
+const kitsuchi={
+    img:"kitsuchi.jpg",
+    title:"KITSUCHI",
+    rank:"JONIN",
+    des:"A high-ranking ninja from the Hidden Stone Village, who is known for his mastery of Earth Release jutsu, and his leadership of the Allied Shinobi Forces during the Fourth Great Ninja War."
+};
+
+const kurotsuchi={
+    img:"kurotsuchi.jpg",
+    title:"KUROTSUCHI",
+    rank:"JONIN",
+    des:" A kunoichi from the Hidden Stone Village, and the granddaughter of the Third Tsuchikage, who possesses a powerful kekkei genkai that allows her to combine Earth and Lava Release jutsu to devastating effect."
+};
+
+const ladychiyo ={
+    img:"lady chiyo.jpg",
+    title:"CHIYOBA SAMA",
+    rank:"VILLAGE ELDER",
+    des:"An elder ninja from the Hidden Sand Village, and the grandmother of Sasori, who possesses a vast knowledge of puppetry and medical ninjutsu, and plays a key role in Sasori's defeat during the Fourth Great Ninja War."
+};
+
+const maki={
+    img:"maki.jpg",
+    title:"MAKI",
+    rank:"JONIN",
+    des:"A kunoichi from the Hidden Leaf Village, and a member of the non-combatant Logistical Support and Medical Division, who possesses exceptional physical strength and is skilled in sealing jutsu."
+};
+
+const mizukage={
+    img:"mizukage.png",
+    title:"MEI TERUMI",
+    rank:"MIZUKAGE",
+    des:"The Fifth Mizukage of the Hidden Mist Village, and a powerful kunoichi with the kekkei genkai ability to use Lava and Boil Release jutsu, as well as being skilled in using her fans as weapons."
+};
+
+const naruto={
+    img:"naruto.jpg",
+    title:"UZZUMAKI NARUTO",
+    rank:"JINCHURIKI",
+    des:": A determined and energetic ninja who never gives up on his dreams of becoming Hokage and making friends along the way."
+};
+
+const nejji ={
+    img:"nejji.jpg",
+    title:"NEJJI",
+    rank:"JONIN",
+    des:"A skilled ninja from the Hidden Leaf Village, and a member of the prestigious Hyuga clan, who is known for his mastery of the Byakugan, a powerful kekkei genkai that allows him to see through solid objects and perceive chakra."
+};
+
+const omui={
+    img:"omoui.jpg",
+    title:"OMUI",
+    rank:"JONIN",
+    des:"A shinobi from the Hidden Cloud Village, and a member of the Allied Shinobi Forces during the Fourth Great Ninja War, who wields a unique sword with a detachable blade that can be used as a shield."
+};
+
+const onoki={
+    img:"onoki.jpg",
+    title:"ONOKI",
+    rank:"TSUCHIKAGE",
+    des:"The Third Tsuchikage of the Hidden Stone Village, and a master of Earth Release jutsu, who possesses immense physical strength despite his small stature, and plays a pivotal role in the Fourth Great Ninja War."
+};
+
+const raikage ={
+    img:"raikage.jpg",
+    title:"RAIKAGE",
+    rank:"RAIKAGE",
+    des:"The leader of the Hidden Cloud Village and one of the five Kage of the Five Great Shinobi Countries, who is known for his exceptional physical strength, speed, and Lightning Release jutsu."
+};
+
+const rocklee ={
+    img:"rock lee sama.jpg",
+    title:"ROCK LEE SAMA",
+    rank:"IDOL",
+    des:"A shinobi from the Hidden Leaf Village who, despite his inability to use ninjutsu and genjutsu, is a master of taijutsu, and is known for his perseverance, work ethic, and unyielding determination."
+}
+const guysensei={
+    img:"guy sensei.jpg",
+    title:"MIGHT GUY",
+    rank:"BLUE BEAST",
+    des:" A shinobi from the Hidden Leaf Village, and a master of taijutsu, who is known for his eccentric personality, his devotion to his students, and his rivalry with Kakashi Hatake."
+};
+const kakashi={
+    img:"kakashi.jpg",
+    title:"SHARINGAN NO KAKASHI",
+    rank:"JONIN",
+    des:" A shinobi from the Hidden Leaf Village, and a highly skilled ninja who is known for his intelligence, mastery of ninjutsu and Sharingan, and his laid-back demeanor."
+};
+
+const sakura={
+    img:"sakura.jpg",
+    title:"SAKURA",
+    rank:"CHUNIN",
+    des:" A shinobi from the Hidden Leaf Village, and a medical ninja who is known for her intelligence, chakra control, and her mastery of the Strength of a Hundred Seal."
+};
+
+const samui={
+    img:"samui.jpg",
+    title:"SAMUI",
+    rank:"JONIN",
+    des:"A shinobi from the Hidden Cloud Village, and the leader of her own team of skilled ninja who specialize in tracking and reconnaissance missions."
+};
+
+const sasori={
+    img:"sasori.jpg",
+    title:"SASORI",
+    rank:"ROGUE",
+    des:"A former shinobi from the Hidden Sand Village, who was known as one of the strongest puppet masters in the world, and who turned himself into a puppet in order to prolong his life."
+};
+
+const sasuke={
+    img:"sasuke.jpg",
+    title:"SASUKE",
+    rank:"ROGUE",
+    des:"A shinobi from the Hidden Leaf Village, and a highly skilled ninja who is known for his intelligence, exceptional talent in ninjutsu and genjutsu, and his dark past."
+    
+};
+const shikamaru ={
+    img:"shikamaru.jpg",
+    title:"SHIKAMARU",
+    rank:"CHUNIN",
+    des:"A shinobi from the Hidden Leaf Village, and a highly intelligent ninja who is known for his analytical mind, strategic thinking, and mastery of shadow techniques."
+};
+
+const shino={
+    img:"shino.jpg",
+    title:"SHINO",
+    rank:"CHUNIN",
+    des:"A shinobi from the Hidden Leaf Village, and a member of the Aburame clan, who specializes in using insects for combat and intelligence gathering."
+};
+
+const temari ={
+    img:"temari.jpg",
+    title:"TEMARI",
+    rank:"JONIN",
+    des:"A shinobi from the Hidden Sand Village, and a highly skilled ninja who specializes in wind-based techniques and long-range combat using her giant folding fan."
+};
+
+const ten_ten={
+    img:"ten ten.jpg",
+    title:"TEN TEN",
+    rank:"CHUNIN",
+    des:" A shinobi from the Hidden Leaf Village, and a weapons specialist who is known for her exceptional skill in using a wide range of weapons, including throwing weapons, swords, and more."
+};
+
+const zabuza ={
+    img:"zabuza.jpg",
+    title:"ZABUZA",
+    rank:"ROGUE",
+    des:"A former shinobi from the Hidden Mist Village, and a highly skilled ninja who is known as the 'Demon of the Hidden Mist' for his brutal combat style and mastery of water-based techniques"
+};
+const haku={
+    img:"haku.jpg",
+    title:"HAKU",
+    rank:"ROGUE",
+    des:"A former shinobi from the Hidden Mist Village, and a highly skilled ninja who is known for his ice-based techniques and speed, as well as his loyalty and devotion to his master, Zabuza Momochi."
+};
+const Konohagakure =[akamaru,choji,guysensei,hinata,ino,itachi,kakashi,naruto,nejji,rocklee,sakura,sasuke,shikamaru,shino,temari,ten_ten];
+const HIDDENMIST =[kisame,mizukage,zabuza,haku];
+const HIDDENSAND=[gaara,kankuro,ladychiyo,maki,sasori];
+const HIDDENROCK=[deidara,omui,kitsuchi,kurotsuchi,onoki];
+const HIDDENCLOUD=[darui,killer_bee,samui,raikage];
+var canvas = document.querySelector(".showboard");
+document.getElementById("leaf").addEventListener("click",leaff);
+document.getElementById("sand").addEventListener("click",sandd);
+document.getElementById("rock").addEventListener("click",rockk);
+document.getElementById("mist").addEventListener("click",mistt);
+document.getElementById("cloud").addEventListener("click",cloudd);
+function leaff (){
+    while(canvas.firstChild){
+        canvas.removeChild(canvas.children[0]);
+    }
+   for(let i=0;i<Konohagakure.length;i++){
+      display(Konohagakure[i]);
+   }
+}
+function sandd (){
+    while(canvas.firstChild){
+        canvas.removeChild(canvas.children[0]);
+    }
+   for(let i=0;i<HIDDENSAND.length;i++){
+      display(HIDDENSAND[i]);
+   }
+}
+function leaff (){
+    while(canvas.firstChild){
+        canvas.removeChild(canvas.children[0]);
+    }
+   for(let i=0;i<Konohagakure.length;i++){
+      display(Konohagakure[i]);
+   }
+}
+function rockk(){
+    while(canvas.firstChild){
+        canvas.removeChild(canvas.children[0]);
+    }
+   for(let i=0;i<HIDDENROCK.length;i++){
+      display(HIDDENROCK[i]);
+   }
+}
+function mistt (){
+    while(canvas.firstChild){
+        canvas.removeChild(canvas.children[0]);
+    }
+   for(let i=0;i<HIDDENMIST.length;i++){
+      display(HIDDENMIST[i]);
+   }
+}
+function cloudd (){
+    while(canvas.firstChild){
+        canvas.removeChild(canvas.children[0]);
+    }
+   for(let i=0;i<HIDDENCLOUD.length;i++){
+      display(HIDDENCLOUD[i]);
+   }
+}
+function display(element){
+    let d1 = document.createDocumentFragment();
+    let div1 = document.createElement('div');
+    div1.classList.add("tryclass");
+        let img = document.createElement('img');
+           img.style.width="240px";
+           img.style.height="100%";
+           img.src=element.img;
+        div1.appendChild(img);
+        let info = document.createElement('div');
+            info.classList.add("info");
+            let heading = document.createElement('div');
+                heading.classList.add("head");
+                let title = document.createElement("p");
+                title.textContent=element.title;
+                let rank = document.createElement("p");
+                rank.textContent=element.rank;
+                rank.style.color=" rgb(222, 155, 10)";
+                heading.appendChild(title);
+                heading.appendChild(rank);
+             info.appendChild(heading);
+             let under = document.createElement("div");
+                under.classList.add("under");
+             info.appendChild(under);
+             let para=document.createElement("p");
+                para.style.justifySelf="flex-end";
+                para.innerText=element.des;
+             info.appendChild(para);
+         div1.appendChild(info);
+    d1.appendChild(div1);
+ canvas.appendChild(d1);
+}
